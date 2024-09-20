df7271e66d17629e1f79cf07bd1e6096d2fc1e57
Siddhant Kapoor <siddhantkapoor2025@gmail.com>
2024-09-20 20:04:32 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <HelmetProvider>
         <CssBaseline />
-        <div onContextMenu={(e) => e.preventDefault()}>
+        <div>
           <App />
         </div>
       </HelmetProvider>
